2d7db75a83ab24841b4cdcdb51fa7466c889676b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-01 16:22:00 +0000

    Added ShiftDTO

---
+++ ShiftsLoggerAPI/Models/ShiftDTO.cs
@@ -0,0 +1,10 @@
+namespace ShiftsLoggerAPI.Models;
+
+public class ShiftDTO
+{
+    public long ShiftId { get; set; }
+    public long EmployeeId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Name { get; set; }
+}
