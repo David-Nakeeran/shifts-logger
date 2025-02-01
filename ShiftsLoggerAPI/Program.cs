d1f70b8bb9b268b74df5ad79977a86779d24c18c
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-01 16:23:04 +0000

    Updated DI container

--- ShiftsLoggerAPI/Program.cs
+++ ShiftsLoggerAPI/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ShiftsLoggerAPI.Data;
+using ShiftsLoggerAPI.Models;
+using ShiftsLoggerAPI.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Get connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddScoped<ShiftMapper>();
+builder.Services.AddScoped<EmployeeMapper>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
