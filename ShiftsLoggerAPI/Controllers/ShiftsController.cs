241cc0cb9bde7db4a70b20b7895cb4970ca8e7b9
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-03 16:33:36 +0000

    formatting

--- ShiftsLoggerAPI/Controllers/ShiftsController.cs
+++ ShiftsLoggerAPI/Controllers/ShiftsController.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShiftsLoggerAPI.Data;
 using ShiftsLoggerAPI.Interface;
 using ShiftsLoggerAPI.Models;
-using ShiftsLoggerAPI.Services;
 
 namespace ShiftsLoggerAPI.Controllers
 {
