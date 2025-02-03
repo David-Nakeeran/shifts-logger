f5a58a7791e61d42c7fbd4988d2e26e50f771d52
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-03 16:32:59 +0000

    shiftid now nullable

--- ShiftsLoggerAPI/Models/EmployeeDTO.cs
+++ ShiftsLoggerAPI/Models/EmployeeDTO.cs
@@ -4,5 +4,5 @@ public class EmployeeDTO
 {
     public long EmployeeId { get; set; }
     public string Name { get; set; }
-    public List<long> ShiftId { get; set; }
+    public List<long>? ShiftId { get; set; }
 }
