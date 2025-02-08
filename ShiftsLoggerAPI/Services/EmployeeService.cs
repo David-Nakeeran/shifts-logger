c6194c9c27bb21652b10574273afeab21770c1ca
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-08 16:11:06 +0000

    Added placeholder methods

--- ShiftsLoggerAPI/Services/EmployeeService.cs
+++ ShiftsLoggerAPI/Services/EmployeeService.cs
@@ -1,5 +1,7 @@
 using ShiftsLoggerAPI.Data;
 using ShiftsLoggerAPI.Interface;
+using ShiftsLoggerAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ShiftsLoggerAPI.Services;
 
@@ -12,5 +14,25 @@ public class EmployeeService : IEmployeeService
         _context = context;
     }
 
+    public async Task<ServiceResponse<List<Employee>>> GetAllEmployeesAsync()
+    {
+
+    }
+    public async Task<ServiceResponse<Employee>> GetEmployeeByIdAsync(long id)
+    {
+
+    }
+    public async Task<ServiceResponse<Employee>> CreateEmployee(EmployeeDTO employeeDTO)
+    {
+
+    }
+    public async Task<ServiceResponse<Employee>> UpdateEmployee(long id, EmployeeDTO employeeDTO)
+    {
+
+    }
+    public async Task<ServiceResponse<Employee>> DeleteEmployee(long id)
+    {
+
+    }
 
 }
