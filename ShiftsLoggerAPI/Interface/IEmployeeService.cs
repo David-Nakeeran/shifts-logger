51a8bc629528f90ac8e0644db32e00cf468168e4
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-09 16:55:21 +0000

    Formatting

--- ShiftsLoggerAPI/Interface/IEmployeeService.cs
+++ ShiftsLoggerAPI/Interface/IEmployeeService.cs
@@ -5,8 +5,12 @@ namespace ShiftsLoggerAPI.Interface;
 public interface IEmployeeService
 {
     public Task<ServiceResponse<List<Employee>>> GetAllEmployeesAsync();
+
     public Task<ServiceResponse<Employee>> GetEmployeeByIdAsync(long id);
+
     public Task<ServiceResponse<Employee>> CreateEmployee(EmployeeDTO employeeDTO);
+
     public Task<ServiceResponse<Employee>> UpdateEmployee(long id, EmployeeDTO employeeDTO);
+
     public Task<ServiceResponse<Employee>> DeleteEmployee(long id);
 }
