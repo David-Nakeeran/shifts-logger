b6c9e29d78c6a4534e2d39fd887b958abb74f518
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-09 16:55:43 +0000

    Updated Two methods: GetAllEmployeesAsync and GetEmployeeByIdAsync

--- ShiftsLoggerAPI/Services/EmployeeService.cs
+++ ShiftsLoggerAPI/Services/EmployeeService.cs
@@ -16,12 +16,52 @@ public class EmployeeService : IEmployeeService
 
     public async Task<ServiceResponse<List<Employee>>> GetAllEmployeesAsync()
     {
+        ServiceResponse<List<Employee>> _response = new ServiceResponse<List<Employee>>();
+
+        try
+        {
+            var employeeList = await _context.Employees
+                .Include(x => x.Shifts)
+                .ToListAsync();
+
+            _response.Success = true;
+            _response.Message = "Ok";
+            _response.Data = employeeList;
+
+        }
+        catch (Exception ex)
+        {
+            _response.Success = false;
+            _response.Message = "Error";
+            _response.Data = null;
+        }
+        return _response;
 
     }
     public async Task<ServiceResponse<Employee>> GetEmployeeByIdAsync(long id)
     {
+        ServiceResponse<Employee> _response = new ServiceResponse<Employee>();
 
+        try
+        {
+            var employee = await _context.Employees
+                .Include(x => x.Shifts)
+                .FirstOrDefaultAsync(employee => employee.EmployeeId == id);
+
+            _response.Success = true;
+            _response.Message = "Ok";
+            _response.Data = employee;
+
+        }
+        catch (Exception ex)
+        {
+            _response.Success = false;
+            _response.Message = "Error";
+            _response.Data = null;
+        }
+        return _response;
     }
+
     public async Task<ServiceResponse<Employee>> CreateEmployee(EmployeeDTO employeeDTO)
     {
 
