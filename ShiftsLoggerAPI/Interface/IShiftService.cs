6ea6ae8305563a0ba40b12e9579b2035072474a1
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-09 16:55:35 +0000

    Formatting

--- ShiftsLoggerAPI/Interface/IShiftService.cs
+++ ShiftsLoggerAPI/Interface/IShiftService.cs
@@ -5,8 +5,12 @@ namespace ShiftsLoggerAPI.Interface;
 public interface IShiftService
 {
     public Task<ServiceResponse<List<Shift>>> GetAllShiftsAsync();
+
     public Task<ServiceResponse<Shift>> GetShiftByIdAsync(long id);
+
     public Task<ServiceResponse<Shift>> CreateShift(ShiftDTO shiftDTO);
+
     public Task<ServiceResponse<Shift>> UpdateShift(long id, ShiftDTO shiftDTO);
+
     public Task<ServiceResponse<Shift>> DeleteShift(long id);
 }
