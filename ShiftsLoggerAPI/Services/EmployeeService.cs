6b3d3cc3fd6c06d7a7b5ffc40e85e56ac70aa8ee
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-10 16:46:33 +0000

    Added all CRUD methods

--- ShiftsLoggerAPI/Services/EmployeeService.cs
+++ ShiftsLoggerAPI/Services/EmployeeService.cs
@@ -48,6 +48,14 @@ public class EmployeeService : IEmployeeService
                 .Include(x => x.Shifts)
                 .FirstOrDefaultAsync(employee => employee.EmployeeId == id);
 
+            if (employee == null)
+            {
+                _response.Success = false;
+                _response.Message = "NotFound";
+                _response.Data = null;
+                return _response;
+            }
+
             _response.Success = true;
             _response.Message = "Ok";
             _response.Data = employee;
@@ -64,15 +72,102 @@ public class EmployeeService : IEmployeeService
 
     public async Task<ServiceResponse<Employee>> CreateEmployee(EmployeeDTO employeeDTO)
     {
+        ServiceResponse<Employee> _response = new ServiceResponse<Employee>();
 
+        try
+        {
+            var employee = new Employee
+            {
+                Name = employeeDTO.Name,
+            };
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            var employeeWithShifts = await _context.Employees
+                .Include(x => x.Shifts)
+                .FirstOrDefaultAsync(e => e.EmployeeId == employee.EmployeeId);
+
+            if (employeeWithShifts == null)
+            {
+                _response.Success = false;
+                _response.Message = "NotFound";
+                _response.Data = null;
+                return _response;
+            }
+
+            _response.Success = true;
+            _response.Message = "Ok";
+            _response.Data = employeeWithShifts;
+        }
+        catch (Exception ex)
+        {
+            _response.Success = false;
+            _response.Message = "Error";
+            _response.Data = null;
+        }
+        return _response;
     }
     public async Task<ServiceResponse<Employee>> UpdateEmployee(long id, EmployeeDTO employeeDTO)
     {
+        ServiceResponse<Employee> _response = new ServiceResponse<Employee>();
+        try
+        {
+            var savedEmployee = await _context.Employees
+                        .Include(x => x.Shifts)
+                        .FirstOrDefaultAsync(x => x.EmployeeId == id);
 
+            if (savedEmployee == null)
+            {
+                _response.Success = false;
+                _response.Message = "NotFound";
+                _response.Data = null;
+                return _response;
+            }
+
+            savedEmployee.Name = employeeDTO.Name;
+            await _context.SaveChangesAsync();
+
+            _response.Success = true;
+            _response.Message = "Updated";
+            _response.Data = savedEmployee;
+        }
+        catch (Exception ex)
+        {
+            _response.Success = false;
+            _response.Message = "Error";
+            _response.Data = null;
+        }
+        return _response;
     }
     public async Task<ServiceResponse<Employee>> DeleteEmployee(long id)
     {
+        ServiceResponse<Employee> _response = new ServiceResponse<Employee>();
+        try
+        {
+            var employee = await _context.Employees.FindAsync(id);
 
+            if (employee == null)
+            {
+                _response.Success = false;
+                _response.Message = "NotFound";
+                _response.Data = null;
+                return _response;
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            _response.Success = true;
+            _response.Message = "Employee deleted successfully";
+            _response.Data = employee;
+        }
+        catch (Exception ex)
+        {
+            _response.Success = false;
+            _response.Message = "Error";
+            _response.Data = null;
+        }
+        return _response;
     }
 
 }
