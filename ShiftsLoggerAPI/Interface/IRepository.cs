ab1a495c50643157f2f8c039919bdd110183e9c0
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-12 16:53:30 +0000

    Added generic repo

---
+++ ShiftsLoggerAPI/Interface/IRepository.cs
@@ -0,0 +1,17 @@
+
+
+namespace ShiftsLoggerAPI.Interface;
+
+public interface IRepository<T> where T : class
+{
+    public Task<List<T>> GetAllAsync();
+
+    public Task<T> GetByIdAsync(long id);
+
+    public Task AddAsync(T entity);
+
+    public Task<T> UpdateAsync(long id, T entity);
+
+    public Task DeleteAsync(long id);
+
+}
