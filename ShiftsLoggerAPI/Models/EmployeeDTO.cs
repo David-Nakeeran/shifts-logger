63f9ebbd7291157e223b2fb60fd44a01d2c99820
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-16 17:09:01 +0000

    Updated DTO

--- ShiftsLoggerAPI/Models/EmployeeDTO.cs
+++ ShiftsLoggerAPI/Models/EmployeeDTO.cs
@@ -3,6 +3,6 @@ namespace ShiftsLoggerAPI.Models;
 public class EmployeeDTO
 {
     public long EmployeeId { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public List<long>? ShiftId { get; set; }
 }
