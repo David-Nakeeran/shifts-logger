3d9c964084096525eb629b87cb5e52f4d337a189
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-16 17:09:33 +0000

    Updated DTO

--- ShiftsLoggerAPI/Models/ShiftDTO.cs
+++ ShiftsLoggerAPI/Models/ShiftDTO.cs
@@ -1,10 +1,19 @@
+using ShiftsLoggerAPI.Validators;
+
 namespace ShiftsLoggerAPI.Models;
 
 public class ShiftDTO
 {
     public long ShiftId { get; set; }
+
     public long EmployeeId { get; set; }
+
     public DateTime StartTime { get; set; }
+
+
+    [EndTimeGreaterThanStartTime(ErrorMessage = "EndTime must be greater than StartTime")]
     public DateTime EndTime { get; set; }
+
+    public string EmployeeName { get; set; }
 
 }
