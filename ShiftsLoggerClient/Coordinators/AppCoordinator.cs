d68da453cac4cb9bfc5ec9f837f68f6924885147
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-17 16:24:44 +0000

    Added switch

--- ShiftsLoggerClient/Coordinators/AppCoordinator.cs
+++ ShiftsLoggerClient/Coordinators/AppCoordinator.cs
@@ -1,11 +1,56 @@
 
 
+using ShiftsLoggerClient.Utilities;
+
 namespace ShiftsLoggerClient.Coordinators;
 
 class AppCoordinator
 {
+    private readonly UserInput _userInput;
+
+    public AppCoordinator(UserInput userInput)
+    {
+        _userInput = userInput;
+    }
     internal async Task Start()
     {
-        Console.WriteLine("App started");
+        bool isAppActive = true;
+
+        while (isAppActive)
+        {
+            var userSelection = _userInput.ShowMenu();
+
+            switch (userSelection)
+            {
+                case "View all employees":
+                    Console.WriteLine("all employees");
+                    break;
+                case "Create employee":
+                    Console.WriteLine("create employee");
+                    break;
+                case "Update employee":
+                    Console.WriteLine("update employee");
+                    break;
+                case "Delete employee":
+                    Console.WriteLine("delete employee");
+                    break;
+                case "View all shifts":
+                    Console.WriteLine("all shifts");
+                    break;
+                case "Create shift":
+                    Console.WriteLine("create shift");
+                    break;
+                case "Update shift":
+                    Console.WriteLine("update shift");
+                    break;
+                case "Delete shift":
+                    Console.WriteLine("delete shift");
+                    break;
+                case "Quit application":
+                    isAppActive = false;
+                    break;
+            }
+
+        }
     }
 }
