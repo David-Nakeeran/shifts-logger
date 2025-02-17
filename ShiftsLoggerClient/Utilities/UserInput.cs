b4b2a8b5bd3a8ab37c9b3dd629491f92ebd76e5d
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-17 16:24:09 +0000

    Added menu selection

--- ShiftsLoggerClient/Utilities/UserInput.cs
+++ ShiftsLoggerClient/Utilities/UserInput.cs
@@ -1,8 +1,39 @@
-
+using Spectre.Console;
 
 namespace ShiftsLoggerClient.Utilities;
 
 public class UserInput
 {
+    public string ShowMenu()
+    {
+        string[] menuOptions = {
+            "View all employees",
+            "Create employee",
+            "Update employee",
+            "Delete employee",
+            "View all shifts",
+            "Create shift",
+            "Update shift",
+            "Delete shift",
+            "Quit application"
+            };
 
+        // Console.Clear();
+        AnsiConsole.MarkupLine("[bold]Main Menu[/]");
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine("[green](Use arrow keys to navigate, then press enter)[/]");
+        AnsiConsole.WriteLine();
+        var userSelection = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Select an option")
+                .AddChoices(menuOptions)
+        );
+        return userSelection;
+    }
+
+    internal void WaitForUserInput()
+    {
+        AnsiConsole.WriteLine("Press any key to continue...");
+        Console.ReadKey(true);
+    }
 }
