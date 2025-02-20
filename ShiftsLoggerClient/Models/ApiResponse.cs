2964e5a0ef22151e18bb62d0841b3a00950e60d9
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-20 16:49:49 +0000

    Added api reponse class

---
+++ ShiftsLoggerClient/Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+using System.Net;
+
+namespace ShiftsLoggerClient.Models;
+
+class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public T? Data { get; set; }
+    public string Message { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+}
