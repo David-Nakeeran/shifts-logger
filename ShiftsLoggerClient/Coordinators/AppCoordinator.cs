b25471d70431747cfb5c4551e4537479cd193516
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-20 16:50:25 +0000

    Refactored getshiftbyid

--- ShiftsLoggerClient/Coordinators/AppCoordinator.cs
+++ ShiftsLoggerClient/Coordinators/AppCoordinator.cs
@@ -1,6 +1,7 @@
 
 
 using ShiftsLoggerClient.Display;
+using ShiftsLoggerClient.Models;
 using ShiftsLoggerClient.Services;
 using ShiftsLoggerClient.Utilities;
 
@@ -62,33 +63,82 @@ class AppCoordinator
         }
     }
 
-    public async Task AllShifts()
+    public async Task<List<ShiftDTO>> GetAllShifts()
     {
         var shifts = await _shiftService.GetAllShifts();
         _displayManager.RenderGetAllShiftsTable(shifts);
+        return shifts;
+    }
+
+    internal async Task AllShifts()
+    {
+        await GetAllShifts();
         _userInput.WaitForUserInput();
     }
 
-    internal async Task GetShiftById()
+    internal async Task<Dictionary<long, long>> GetKeyValuePairs()
     {
-        var shifts = await _shiftService.GetAllShifts();
-        _displayManager.RenderGetAllShiftsTable(shifts);
-        _userInput.WaitForUserInput();
-        var displayId = _userInput.GetId();
-        int count = 1;
-        long shiftId;
+        var shifts = await GetAllShifts();
+        var keyValuePairs = new Dictionary<long, long>();
+        long displayId = 1;
+
         foreach (var shift in shifts)
         {
-            if (count == displayId)
-            {
-                shiftId = shift.ShiftId;
-                var shiftById = await _shiftService.GetShiftById(shiftId);
-                _displayManager.RenderGetShiftByIdTable(shiftById);
-                _userInput.WaitForUserInput();
-            }
-            count++;
-
+            keyValuePairs[displayId] = shift.ShiftId;
+            displayId++;
         }
+        return keyValuePairs;
+    }
 
+    internal async Task<ShiftDTO> GetShiftById()
+    {
+        var shifts = await GetAllShifts();
+        _userInput.WaitForUserInput();
+
+        var displayId = _userInput.GetId();
+        var pairs = await GetKeyValuePairs();
+        long shiftId;
+
+        foreach (var element in pairs)
+        {
+            if (element.Key == displayId)
+            {
+                shiftId = element.Value;
+                return await _shiftService.GetShiftById(shiftId);
+            }
+        }
+        return new ShiftDTO { };
+        // _displayManager.RenderGetShiftByIdTable(shiftById);
+        // _userInput.WaitForUserInput();
+
+
+        // int count = 1;
+        // long shiftId;
+        // foreach (var shift in shifts)
+        // {
+        //     if (count == displayId)
+        //     {
+        //         shiftId = shift.ShiftId;
+        //         var shiftById = await _shiftService.GetShiftById(shiftId);
+        //         _displayManager.RenderGetShiftByIdTable(shiftById);
+        //         _userInput.WaitForUserInput();
+        //     }
+        //     count++;
+
+        // }
+    }
+
+    internal async Task DeleteShift()
+    {
+        var result = await _shiftService.DeleteShiftById(shiftId);
+
+        if (result.Success)
+        {
+            // display manager method to show message on success
+        }
+        else
+        {
+            // display manager method to show error status code and message
+        }
     }
 }
