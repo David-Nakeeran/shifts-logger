e499fa8720058221ebf96ae119e5fa9d65b95d1b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-25 16:31:31 +0000

    Updated interface

--- ShiftsLoggerAPI/Interface/IRepository.cs
+++ ShiftsLoggerAPI/Interface/IRepository.cs
@@ -1,17 +1,6 @@
-
-
 namespace ShiftsLoggerAPI.Interface;
 
 public interface IRepository<T> where T : class
 {
-    public Task<List<T>> GetAllAsync();
-
-    public Task<T> GetByIdAsync(long id);
-
-    public Task AddAsync(T entity);
-
-    public Task<T> UpdateAsync(long id, T entity);
-
     public Task<bool> DeleteAsync(long id);
-
 }
