22a78900e06302ee3c896f8f94b9e179cc60e1d5
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-26 13:36:10 +0000

    formatting

--- ShiftsLoggerClient/Models/ApiResponse.cs
+++ ShiftsLoggerClient/Models/ApiResponse.cs
@@ -5,7 +5,10 @@ namespace ShiftsLoggerClient.Models;
 class ApiResponse<T>
 {
     public bool Success { get; set; }
+
     public T? Data { get; set; }
+
     public string Message { get; set; }
+
     public HttpStatusCode StatusCode { get; set; }
 }
