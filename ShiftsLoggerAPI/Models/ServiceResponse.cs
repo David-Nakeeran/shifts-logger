08623d64d2e9200e45feab7dd701a041b900b653
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-26 13:59:50 +0000

    formatting

--- ShiftsLoggerAPI/Models/ServiceResponse.cs
+++ ShiftsLoggerAPI/Models/ServiceResponse.cs
@@ -3,7 +3,9 @@ namespace ShiftsLoggerAPI.Models;
 public class ServiceResponse<T>
 {
     public T? Data { get; set; }
+
     public bool Success { get; set; } = true;
+
     public string Message { get; set; } = null;
 
     public string Error { get; set; } = null;
