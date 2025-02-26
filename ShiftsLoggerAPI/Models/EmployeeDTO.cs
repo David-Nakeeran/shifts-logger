900fc066d4282df24d7366149c7a9443305a564b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-02-26 14:00:02 +0000

    formatting

--- ShiftsLoggerAPI/Models/EmployeeDTO.cs
+++ ShiftsLoggerAPI/Models/EmployeeDTO.cs
@@ -3,6 +3,8 @@ namespace ShiftsLoggerAPI.Models;
 public class EmployeeDTO
 {
     public long EmployeeId { get; set; }
+
     public string? Name { get; set; }
+
     public List<long>? ShiftId { get; set; }
 }
